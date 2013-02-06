c6a3bc16500a33acbbaa3390237ad6b2f8372b4e
Kiril Staikov <kstaikov@gmail.com>
2013-02-06 11:06:12 -0500

    renamed folder

---
+++ Projects/Virtual Reality/Bananarchy/0.3.0/bananarchy_0.3.py
@@ -0,0 +1,455 @@
+####################################################
+####################################################
+#####################BANANARCHY#####################
+##############Written by Kiril Staikov##############
+####################################################
+####################################################
+
+from pandaepl.common import *
+import os
+from numpy import *
+import numpy
+import ctypes
+nidaq = ctypes.windll.nicaiu
+import time
+import random
+
+class bananarchy:
+
+    def __init__(self):
+        """
+        Initialize the experiment.
+        """
+        
+        exp = Experiment.getInstance() #Get experiment instance.        
+        vr = Vr.getInstance() #Get VR environment object.
+
+        # Set Session# to +1 the # of sessions the subject has data for so far. 
+	if not os.access(exp.getExpDirectory(), os.F_OK):
+	        exp.setSessionNum(0)
+	else:
+		i = 0
+		while os.access(exp.getExpDirectory() + '/session_' + str(i), os.F_OK):
+			i = i + 1
+		exp.setSessionNum(i) 
+
+        config = Conf.getInstance().getConfig() #Get configuration dictionary.
+        avatar = Avatar.getInstance()
+        self.nidaqSetup() #Setup the basic NI-DAQ Hardware Controls
+        self.loadEnvironment(config) #Load environment.
+
+        # Set up fog.
+        if int(config['training'] == 4):
+            self.fogScheme = config['initialFogScheme']
+            self.setUpFog()
+        
+        # Register Custom Log Entries
+       	Log.getInstance().addType("YUMMY",[("BANANA",basestring)], False) #This one corresponds to colliding with a banana.
+        
+        # Create crosshairs around what seems to be the center of the screen
+	self.cross = Text("cross", '+', Point3(-.08,0,.1), .3, Point4(1,0,0,1), config['textFont'])
+		
+        # Register tasks to be performed between frames
+        vr.addTask(Task("pumpOut",
+                        lambda taskInfo: 
+                          self.pumpOut(config), 
+                        config['pulseInterval'])) # Outputs pulses to pump when reward is called for.
+
+        vr.addTask(Task("trackHeader", 
+                        lambda taskInfo: 
+                          self.trackHeader(config, avatar))) # Monitors header position and calls for reward when necessary.
+                   
+        # Register the handling of keyboard events.
+        vr.inputListen("toggleDebug", 
+                       lambda inputEvent: 
+                         Vr.getInstance().setDebug(not Vr.getInstance().isDebug()))
+	vr.inputListen("restart", self.restart)
+	if config['training'] > 0:
+		vr.inputListen("left", self.left)
+		vr.inputListen("center", self.center)
+		vr.inputListen("right", self.right)
+		vr.inputListen("increaseDist", self.increaseDist)
+		vr.inputListen("decreaseDist", self.decreaseDist)
+		vr.inputListen("toggleFog", self.toggleFog)
+		vr.inputListen("decreaseFog", self.decreaseFog)
+
+        # Starting banana location
+        if config['training'] == 3.1:
+            self.center(self)
+        elif ((config['training'] == 1.1) | (random.randrange(0, 2) > 0)) & (config['training'] != 1.2):
+            self.left(self, config)
+        else:
+            self.right(self, config)
+
+        self.didit = 0
+        
+    def nidaqSetup(self):
+        """
+        Setup basic NI-DAQ hardware controls
+        """
+
+        #Set ctypes
+        self.int32 = ctypes.c_long
+        self.uInt32 = ctypes.c_ulong
+        self.uInt64 = ctypes.c_ulonglong
+        self.float64 = ctypes.c_double
+        self.bool32 = ctypes.c_bool
+        self.TaskHandle = self.uInt32
+
+        #Import certain constants defined in the NI-DAQ API header file
+        self.DAQmx_Val_Cfg_Default = self.int32(-1)
+        self.DAQmx_Val_RSE = 10083
+        self.DAQmx_Val_Volts = 10348
+        self.DAQmx_Val_Rising = 10280
+        self.DAQmx_Val_FiniteSamps = 10178
+        self.DAQmx_Val_GroupByChannel = 0
+        
+        
+        #Initialize variables
+        self.taskHandle = self.TaskHandle(0)
+
+        #Set some basic variables that control the reward pulses.
+        self.Rate = 115
+        self.samps = 3
+        self.data = numpy.zeros( ( self.samps, ), dtype=numpy.float64 )
+        self.beeps = 0   #Beep/Pulse counter
+        self.reward = 2  #When set to 1, reward output is triggered.
+
+        #Setup NI-DAQ task
+        nidaq.DAQmxCreateTask("",
+                ctypes.byref( self.taskHandle ))
+        nidaq.DAQmxCreateAOVoltageChan( self.taskHandle,
+                                           "Dev1/ao0",
+                                           "",
+                                           self.float64(3.0),
+                                           self.float64(10.0),
+                                           self.DAQmx_Val_Volts,
+                                           None)
+        nidaq.DAQmxCfgSampClkTiming( self.taskHandle,
+                                        "",
+                                        self.float64(self.Rate),
+                                        self.DAQmx_Val_Rising,
+                                        self.DAQmx_Val_FiniteSamps,
+                                        self.uInt64(self.samps));
+
+
+        nidaq.DAQmxWriteAnalogF64( self.taskHandle,
+                                      self.int32(self.samps),
+                                      0,
+                                      self.float64(-1),
+                                      self.DAQmx_Val_GroupByChannel,
+                                      self.data.ctypes.data,
+                                      None,
+                                      None)        
+
+    def loadEnvironment(self, config):
+        """
+        Load terrain, sky, and bananas.
+        """
+        
+        if (config['training'] == 0) or (int(config['training']) == 4):       
+            # Load terrain.
+            self.terrainModel = Model("terrain", config['terrainModel'], 
+                                      config['terrainCenter'])
+
+            # When hitting an object that is part of the terrain, repel or slide?
+            self.terrainModel.setCollisionCallback(MovingObject.handleRepelCollision)
+
+            # Load sky.
+            self.skyModel = Model("sky", config['skyModel'])
+            self.skyModel.setScale(config['skyScale'])
+
+
+        # Load bananas.
+        self.bananaModels = []
+        for i in range(0, config['numBananas']):
+            bananaModel = Model("banana"+str(i), 
+                               os.path.join(config['bananaDir'],"banana"+".bam"),
+                               Point3(config['bananaLocs'][i][0], 
+                                      config['bananaLocs'][i][1], 
+                                      config['bananaZ']),
+                               self.collideBanana)
+	    bananaModel.setScale(config['bananaScale'])
+            self.bananaModels.append(bananaModel)
+	    if config['training'] > 0:
+		self.bananaModels[i].setStashed(1)
+
+    
+    def setUpFog(self):
+        """
+        Set up fog according to the current scheme.
+        """
+        # Get configuration dictionary.
+        config = Conf.getInstance().getConfig()
+
+        # Delete previously set fog.
+        self.fog = None
+
+        # No fog.
+        if self.fogScheme == 0:
+            pass
+        # Exponential fog.
+        elif self.fogScheme >= 1:
+            self.fog = ExpFog("demoExpFog", config['expFogColor'], 
+                              config['expFogDensity'] * (.9**self.fogScheme))
+
+        # Display fog everywhere.
+        #Vr.getInstance().setFog(self.fog)
+        self.skyModel.setFog(self.fog)
+        self.terrainModel.setFog(self.fog)
+
+    def toggleFog(self, inputEvent):
+        """
+        Cycle between different fog schemes.
+        """
+        TOTAL_FOG_SCHEMES = 70
+        if self.fogScheme > 0:
+            self.fogScheme    = (self.fogScheme+1) % TOTAL_FOG_SCHEMES
+        print(self.fogScheme)
+        self.setUpFog()
+
+    def decreaseFog(self, inputEvent):
+        """
+        Cycle between different fog schemes.
+        """
+        TOTAL_FOG_SCHEMES = 70
+        if self.fogScheme != 1:
+            self.fogScheme    = (self.fogScheme-1) % TOTAL_FOG_SCHEMES
+        print(self.fogScheme)
+        self.setUpFog()
+
+    def restart(self, inputEvent):
+	'''
+	Reload config file, give bananas new locations, make them visible, set new session number
+	and return the subject to the original location, with no movement/momentum in any direction.
+	'''
+	
+	config = Conf.getInstance().getConfig()
+
+        self.cross.setColor(Point4(1, 0, 0, 1))
+        self.bananaModels[1].setStashed(1)
+        self.bananaModels[2].setStashed(1)
+        self.bananaModels[0].setStashed(1)
+        if config['training'] == 3.1:
+            self.center(self)
+        else:
+            randSide = random.randrange(0, 2)
+            if randSide < 1:
+                self.left(self, config)
+            else:
+                self.right(self, config)
+	avatar = Avatar.getInstance()
+	avatar.setLinearAccel(0)
+	avatar.setLinearSpeed(0)
+	avatar.setTurningSpeed(0)
+	avatar.setTurningAccel(0)	
+	avatar.setH(0)
+	avatar.setPos(Point3(0, 0, 0.5))
+	avatar.setMaxTurningSpeed(config['fullTurningSpeed'])
+	self.didit = 0
+	
+    def left(self, inputEvent, config):
+	self.bananaModels[1].setStashed(1)
+	self.bananaModels[2].setStashed(1)
+	if self.bananaModels[0].isStashed() == 0:
+		self.bananaModels[0].setStashed(1)
+		self.targetHeader = float('nan')
+	else:
+                self.bananaModels[0].setPos(Point3(random.uniform(-config['minDistance'], -config['maxDistance']), 2, 1))
+		self.bananaModels[0].setStashed(0)
+                a = self.bananaModels[0].getPos()[0]
+                b = self.bananaModels[0].getPos()[1]
+                self.targetHeader = rad2deg(arccos(b/sqrt(square(a) + square(b))))
+
+
+    def center(self, inputEvent):
+	self.bananaModels[0].setStashed(1)
+	self.bananaModels[2].setStashed(1)
+	if self.bananaModels[1].isStashed() == 0:
+		self.bananaModels[1].setStashed(1)
+		self.targetHeader = float('nan')
+	else:
+		self.bananaModels[1].setStashed(0)
+                a = self.bananaModels[1].getPos()[0]
+                b = self.bananaModels[1].getPos()[1]
+                self.targetHeader = rad2deg(arccos(b/sqrt(square(a) + square(b))))
+
+    def right(self, inputEvent, config):
+	self.bananaModels[0].setStashed(1)
+	self.bananaModels[1].setStashed(1)
+	if self.bananaModels[2].isStashed() == 0:
+		self.bananaModels[2].setStashed(1)
+		self.targetHeader = float('nan')
+	else:
+                self.bananaModels[2].setPos(Point3(random.uniform(config['minDistance'], config['maxDistance']), 2, 1))
+		self.bananaModels[2].setStashed(0)
+                a = self.bananaModels[2].getPos()[0]
+                b = self.bananaModels[2].getPos()[1]
+                self.targetHeader = -rad2deg(arccos(b/sqrt(square(a) + square(b))))
+
+    def increaseDist(self, inputEvent):
+        self.bananaModels[0].setPos(Point3(self.bananaModels[0].getPos()[0] - .05, 2, 1))
+        a = self.bananaModels[0].getPos()[0]
+        b = self.bananaModels[0].getPos()[1]
+        if not self.bananaModels[0].isStashed():
+            self.targetHeader = rad2deg(arccos(b/sqrt(square(a) + square(b))))
+
+        self.bananaModels[2].setPos(Point3(self.bananaModels[2].getPos()[0] + .05, 2, 1))
+        a = self.bananaModels[2].getPos()[0]
+        b = self.bananaModels[2].getPos()[1]
+        if not self.bananaModels[2].isStashed():
+            self.targetHeader = -rad2deg(arccos(b/sqrt(square(a) + square(b))))
+
+    def decreaseDist(self, inputEvent):
+        self.bananaModels[0].setPos(Point3(self.bananaModels[0].getPos()[0] + .05, 2, 1))
+        a = self.bananaModels[0].getPos()[0]
+        b = self.bananaModels[0].getPos()[1]
+        if not self.bananaModels[0].isStashed():
+            self.targetHeader = rad2deg(arccos(b/sqrt(square(a) + square(b))))
+
+        self.bananaModels[2].setPos(Point3(self.bananaModels[2].getPos()[0] - .05, 2, 1))
+        a = self.bananaModels[2].getPos()[0]
+        b = self.bananaModels[2].getPos()[1]
+        if not self.bananaModels[2].isStashed():
+            self.targetHeader = -rad2deg(arccos(b/sqrt(square(a) + square(b))))
+	
+    def start(self):
+        """
+        Start the experiment.
+        """
+
+        Experiment.getInstance().start()
+        
+    def collideBanana(self, collisionInfoList):
+        """
+        Handle the participant colliding with a banana.
+        """
+        config = Conf.getInstance().getConfig()
+        
+        # ID of the banana the participant collided with.
+        banana = collisionInfoList[0].getInto().getIdentifier()
+
+        # Log collision.
+        VLQ.getInstance().writeLine("YUMMY", [banana])
+            
+        # Upon touching banana, repel movement.  Banana disappears.
+        MovingObject.handleRepelCollision(collisionInfoList)
+	
+
+        # Reward
+
+	#self.reward = 1
+        if config['training'] < 3.1:
+            self.targetHeader = float('nan')
+	self.cross.setColor(Point4(0, 1, 0, 1))
+        self.reward = 1
+
+	#collisionInfoList[0].getInto().setStashed(1)
+        
+    def pumpOut(self, config):
+        """
+        Carry out reward delivery process when self.reward is set to 1.
+        """
+       
+        if (self.beeps < config['numBeeps']) & (self.reward == 1):
+            nidaq.DAQmxStartTask( self.taskHandle )
+            nidaq.DAQmxWaitUntilTaskDone( self.taskHandle, self.float64(-1) )
+            nidaq.DAQmxStopTask( self.taskHandle )
+            self.beeps = self.beeps + 1
+            if self.beeps == 6:
+                self.didit = 1
+        else:                
+            self.beeps = 0
+            self.reward = 0
+            if self.cross.getColor()[1] == 1:
+                self.restart(self)
+                
+                           
+    def trackHeader(self, config, avatar):
+        """
+        Monitor the header between every frame and perform the relevant
+        header-dependent functions.
+        """
+
+        hedder = avatar.getH()
+        
+        #Maintain boundaries to ensure Avatar never rotates so much that banana disappears from screen
+        if (self.bananaModels[0].isStashed() == 0) & (-hedder >= (config['FOV']/2 - 1) - abs(self.targetHeader)):
+            avatar.setLinearAccel(0)
+            avatar.setLinearSpeed(0)
+            avatar.setTurningSpeed(0)
+            avatar.setTurningAccel(0)
+            avatar.setH(hedder+.02)
+        elif (self.bananaModels[2].isStashed() == 0) & (hedder >= (config['FOV']/2 - 1) - abs(self.targetHeader)):
+            avatar.setLinearAccel(0)
+            avatar.setLinearSpeed(0)
+            avatar.setTurningSpeed(0)
+            avatar.setTurningAccel(0)
+            avatar.setH(hedder-.02)
+
+        if (self.bananaModels[0].isStashed() == 0) & (hedder >= (config['FOV']/2 - 1) + self.targetHeader):
+            avatar.setLinearAccel(0)
+            avatar.setLinearSpeed(0)
+            avatar.setTurningSpeed(0)
+            avatar.setTurningAccel(0)
+            avatar.setH(hedder-.02)
+        elif (self.bananaModels[2].isStashed() == 0) & (hedder <= -(config['FOV']/2 - 1) + self.targetHeader):
+            avatar.setLinearAccel(0)
+            avatar.setLinearSpeed(0)
+            avatar.setTurningSpeed(0)
+            avatar.setTurningAccel(0)
+            avatar.setH(hedder+.02)
+            
+        #What to do when the current header matches the target header:
+        #Reward, make crosshairs blue, and freeze rotation until reward is complete.  Then hide banana, reset position,
+        #allow rotation, and present new target banana in random location.
+        if (hedder < self.targetHeader + config['targetHwinL']) & (hedder > self.targetHeader - config['targetHwinR']):
+            if int(config['training']) < 3:
+                self.reward = 1
+            if (config['training'] >= 3.2) & (self.didit == 0) & (self.beeps < 6):
+                self.reward = 1
+                #self.didit = 1
+            elif self.cross.getColor()[1] == 1:
+                self.reward = 1
+            elif config['training'] >= 3.2:
+                self.reward = 0
+
+            if self.cross.getColor()[1] != 1:
+                self.cross.setColor(Point4(0, 0, 1, 1))
+              
+            avatar.setMaxForwardSpeed(config['fullForwardSpeed'])
+
+            if (int(config['training']) == 1) or (config['training'] == 3.2) or ((config['training'] >= 3.3) and (avatar.getPos()[1] > 0)):
+                avatar.setMaxTurningSpeed(0)
+            if (self.beeps >= config['numBeeps']) & (config['training'] < 3.1):
+                self.bananaModels[0].setStashed(1)
+                self.bananaModels[2].setStashed(1)
+                avatar.setH(0)
+		if ((config['training'] == 1.1) | (random.randrange(0, 2) < 1)) & (config['training'] != 1.2):
+			self.bananaModels[0].setPos(Point3(random.uniform(-config['minDistance'], -config['maxDistance']), 2, 1))
+			a = self.bananaModels[0].getPos()[0]
+		        b = self.bananaModels[0].getPos()[1]
+		        self.targetHeader = rad2deg(arccos(b/sqrt(square(a) + square(b))))
+			self.bananaModels[0].setStashed(0)
+		else: 
+			self.bananaModels[2].setPos(Point3(random.uniform(config['minDistance'], config['maxDistance']), 2, 1))
+		        a = self.bananaModels[2].getPos()[0]
+		        b = self.bananaModels[2].getPos()[1]
+		        self.targetHeader = -rad2deg(arccos(b/sqrt(square(a) + square(b))))	
+                        self.bananaModels[2].setStashed(0)
+
+                self.cross.setColor(Point4(1, 0, 0, 1))
+                avatar.setMaxTurningSpeed(config['fullTurningSpeed'])
+        else:
+            if config['training'] >= 3.3:
+                self.reward = 0
+            if self.reward == 0:
+                self.cross.setColor(Point4(1, 0, 0, 1))
+                avatar.setMaxForwardSpeed(0)
+            if int(config['training']) <= 3:
+                if self.beeps >= 5:
+                    self.reward = 0
+                    
+
+# Create a new instance of the experiment and start it up.
+bananarchy().start()
---
+++ Projects/Virtual Reality/Bananarchy/0.3.0/config.py
@@ -0,0 +1,183 @@
+####################################################
+####################################################
+#####################BANANARCHY#####################
+##############Written by Kiril Staikov##############
+####################################################
+####################################################
+#
+#Training Steps? 
+#  0 = Final Task [BROKEN].
+#
+#1.x = Left-Right Training.  No background.  Crosshairs in center of screen,
+#      and banana appears on left or right of crosshairs.  Subject has to
+#      push joystick to right or left to align the crosshairs with the
+#      bannaa.  Once the crosshairs hit the banana, the position is locked in place
+#      until all the reward is dispensed.  Then the tasks restarts with the banana
+#      in a new random location.
+#1.1 = Left only. [Perhaps need to add a variable to 
+#      control max distance, and vary within that.  That's what
+#      increaseDistance and decreaseDistance should do.]
+#1.2 = Right only. [Perhaps need to add a variable to 
+#      control max distance, and vary within that.  That's what
+#      increaseDistance and decreaseDistance should do.]
+#1.3 = Both, randomized location and distance.
+#
+#2.x = Continuation of the left-right training in 1.x.  When the crosshairs
+#      align wih the target, some reward is dispensed, but the positions
+#      don't lock in and the crosshairs are allowed to leave the target, at
+#      which point reward stops being dispensed, until the crosshairs and
+#      target are aligned again.  The goal is to get the animal to leave
+#      the crosshairs 
+#2.1 = The window within which the crosshair has to fall is fairly large
+#      Window to Left of Banana = 4; Right = 6.  They are different for technical
+#      reasons I won't go into here.
+#2.2 = Left = 3; Right = 5.
+#2.3 = Left = 2; Right = 4.
+#2.4 = Left = 1; Right = 3;
+#2.5 = Left = 1; Right = 2; This is the final window.
+#
+#3.x = Introducing forward movement. 
+#3.1 = Start out with banana centered, just go forward to get reward.[BROKEN.  Forward has to remain pressed.] 
+#3.2 = Left-right to banana, with crosshairs on it, then go forward to get rewarded again.
+#      Forward movement is blocked until crosshair turns blue.  When crosshairs hit banana,
+#      further turning is blocked.  Only forward movement is allowed.
+#3.3 = Require him to stop on his own on the banana.  As soon as he moves forward a bit,
+#      left and right turning is blocked, only more forward movement is allowed.    He is
+#      rewarded up to 6 beeps at a time for turning until the crosshairs are blue, and then
+#      he can only get more reward by actually going up to the banana.
+#
+#4.x = Introducing the environment. 
+#4.0 = Starts off the same place as 3.3. Except the background is present and there is a massive,
+#      and very dense fog over it.  Press F to remove the fog and eluminate the environment/backround
+#      little by little.
+#
+#============================================================================================================
+#
+#
+#TO DO:
+#------------
+#1.) Figure out what to do with the window sizes for each training step, the speeds, etc.  
+#2.) For the training steps, use only one banana rather than alternate between the three banana models on
+#    left, right, and center, and adjust the corresponding functions appropriately.  Just change the positions
+#    of the single banana.
+#3.) Use assignment operators where possible
+#4.) Use modulus if it would improve anything.
+#5.) Use words for the logical operators for the sake of readability.
+#6.) Use membership operators where possible, if they would improve anything.
+
+training = 4
+
+from random import uniform
+
+##########################
+# Core PandaEPL settings.#
+##########################
+
+FOV = 60
+
+# Movement.
+linearAcceleration  = 250
+if int(training) >= 3:
+        fullForwardSpeed = 50
+else:
+        fullForwardSpeed    = 0
+fullBackwardSpeed   = 0 
+turningAcceleration = 50
+if training == 3.1:
+        fullTurningSpeed = 0
+elif training >= 2:
+        fullTurningSpeed    = 13
+else:
+        fullTurningSpeed = 200
+turningLinearSpeed  = 2 # Factor
+
+maxTurningLinearSpeed          = 90.0
+minTurningLinearSpeedReqd      = 1.0
+minTurningLinearSpeed          = 1.5
+minTurningLinearSpeedIncrement = 0.5 
+
+initialPos   = Point3(0, 0, 0.5)
+avatarRadius = 0.5
+cameraPos    = Point3(0, 0, 0.5)
+friction     = .4
+movementType = 'walking' # car | walking
+
+# Instructions.
+instructSize    = 0.075
+#instructFont    = '/usr/share/fonts/truetype/freefont/FreeSans.ttf';  #Linux
+instructFont    = '/c/Windows/Fonts/times.ttf';                      # Windows
+instructBgColor = Point4(0, 0, 0, 1)
+instructFgColor = Point4(1, 1, 1, 1)
+instructMargin  = 0.06
+instructSeeAll  = False
+
+################################
+# Experiment-specific settings.#
+################################
+# Font
+textFont = '/c/Windows/Fonts/times.ttf' # Used for the crosshairs plus sign
+
+# Bananas.
+bananaDir  = './models/bananas/'
+bananaZ    = 1
+bananaScale = .5
+bananaLocs = []
+numBananas = 100
+for i in range(0, numBananas):
+	x = uniform(-12, 12)
+	y = uniform(-15.5, 15.5)
+	bananaLocs.append([x, y, 90])
+
+if training > 0:
+	numBananas = 3
+	distance = .15
+	bananaLocs[0] = [initialPos[0] - distance, initialPos[1] + 2, 90]
+	bananaLocs[1] = [initialPos[0], initialPos[1] + 2, 90]
+	bananaLocs[2] = [initialPos[0] + distance, initialPos[1] + 2, 90]
+
+minDistance = .25
+maxDistance = .7
+
+# Target header window, to the left and right
+if (training > 2) & (training < 2.5):
+        targetHwinL = 4 - (((training - 2) * 10) - 1)
+        targetHwinR = 4 - (((training - 2) * 10) - 1)
+else:
+        targetHwinL = 4 #1.2 is sort of the boundary.
+        targetHwinR = 4 #1.2 is sort of the boundary.
+
+# Terrain, sky.
+terrainModel  = './models/towns/final.bam'
+terrainCenter = Point3(0,0,0)
+skyModel      = './models/sky/sky.bam'
+skyScale      = 1.6
+
+# Some reward pump parameters.
+pulseInterval = 200 # in ms
+if int(training) == 2:
+        numBeeps = 50 # Number of pulses/beeps in typical reward sequence
+else:
+        numBeeps = 20
+
+# (Non-default) command keys.
+keyboard = Keyboard.getInstance()
+keyboard.bind("exit", ["escape", "q"])
+keyboard.bind("toggleDebug", ["escape", "d"])
+keyboard.bind("left", "l")
+keyboard.bind("right", "r")
+keyboard.bind("restart", "y")
+keyboard.bind("center", "c")
+keyboard.bind("increaseDist", "i")
+keyboard.bind("decreaseDist", "d")
+
+if int(training) == 4:
+        keyboard.bind("toggleFog", "f")
+        keyboard.bind("decreaseFog", "v")
+
+joystick = Joystick.getInstance()
+joystick.bind("toggleDebug", "joy_button0")
+
+# Fog.
+initialFogScheme  = 1
+expFogColor       = Point3(0.4,0.4,0.4)
+expFogDensity     = 1
